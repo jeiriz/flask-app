bb37046e9a172dd7f6fd047ed43b1f754812c8c7
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2017-10-06 21:01:32 -0300

    Agregando mil features

--- app.py
+++ app.py
@@ -1,6 +1,76 @@
-from flask import Flask
-app = Flask(__name__)
+#!/usr/bin/env python
+import csv
+from datetime import datetime
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_script import Manager
+from forms import LoginForm, SaludarForm, RegistrarForm
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+app.config['SECRET_KEY'] = 'un string que funcione como llave'
+
+@app.route('/')
+def index():
+    return render_template('index.html', fecha_actual=datetime.utcnow())
+
+@app.route('/saludar', methods=['GET', 'POST'])
+def saludar():
+    formulario = SaludarForm()
+    if formulario.validate_on_submit():
+        print(formulario.usuario.name)
+        return redirect(url_for('saludar_persona', usuario=formulario.usuario.data))
+    return render_template('saludar.html', form=formulario)
+
+@app.route('/saludar/<usuario>')
+def saludar_persona(usuario):
+    return render_template('usuarios.html', nombre=usuario)
+
+@app.errorhandler(404)
+def no_encontrado(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def error_interno(e):
+    return render_template('500.html'), 500
+
+@app.route('/ingresar', methods=['GET', 'POST'])
+def ingresar():
+    formulario = LoginForm()
+    if formulario.validate_on_submit():
+        with open('usuarios') as archivo:
+            archivo_csv = csv.reader(archivo)
+            registro = next(archivo_csv)
+            while registro:
+                if formulario.usuario.data == registro[0] and formulario.password.data == registro[1]:
+                    flash('Bienvenido')
+                    return render_template('ingresado.html')
+                registro = next(archivo_csv, None)
+            else:
+                flash('Revisá nombre de usuario y contraseña')
+                return redirect(url_for('ingresar'))
+    return render_template('login.html', formulario=formulario)
+
+
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    formulario = RegistrarForm()
+    if formulario.validate_on_submit():
+        if formulario.password.data == formulario.password_check.data:
+            with open('usuarios', 'a+') as archivo:
+                archivo_csv = csv.writer(archivo)
+                registro = [formulario.usuario.data, formulario.password.data]
+                archivo_csv.writerow(registro)
+            flash('Usuario creado correctamente')
+            return redirect(url_for('ingresar'))
+        else:
+            flash('Las passwords no matchean')
+    return render_template('registrar.html', form=formulario)
+
+if __name__ == "__main__":
+    # app.run(host='0.0.0.0', debug=True)
+    manager.run()
